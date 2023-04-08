ad6310829672cbec9892ff3142f7fa4ceee6f634
Souvik Das <souvikpunk@gmail.com>
2023-04-08 12:03:23 +0530

    added navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  template: '<h1>Navbar Component</h1>',
+  templateUrl: 'navbar.component.html',
 })
 export class NavbarComponent {
   constructor() {}
