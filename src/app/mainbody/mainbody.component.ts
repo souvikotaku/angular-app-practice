61a8f1bf3278297c11bfbe2c4c829198279e9a4b
Souvik Das <souvikpunk@gmail.com>
2023-04-10 12:39:53 +0530

    added boolean condition

--- src/app/mainbody/mainbody.component.ts
+++ src/app/mainbody/mainbody.component.ts
@@ -31,5 +31,10 @@ export class MainBody implements AfterViewInit {
     console.log('received mesage on btn click', $event);
     this.receivedmsgfrombtn = $event;
   }
+  imgUrl: string =
+    'https://cdn.theatlantic.com/thumbor/9R5-6pI2aAgktJkscy2zisDfr8s=/1056x0:2664x1608/1080x1080/media/img/mt/2021/06/tra8660_105_trl_comp_v004_7f1c1cb3a/original.jpg';
+
+  redBool: boolean = true;
+  redBool2: boolean = false;
   // ngOnInit(): void {}
 }
